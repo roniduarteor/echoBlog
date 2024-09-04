42dee4b5cbc583609295b1ac3b90458299e20aac
Ronaldo Duarte <ronaldoduarteoliveirarodrigues@gmail.com>
2024-09-04 09:53:11 -0300

    :sparkles: Feat: RF02 Concluída!

--- backend/src/controllers/postagemController.js
+++ backend/src/controllers/postagemController.js
@@ -36,3 +36,30 @@ export const create = async (request, response) => {
         response.status(500).json({ message: "Erro ao fazer uma nova Postagem" })
     }
 }
+
+// postagens?page1&limit=3
+export const getAll = async (request, response) => {
+    const page = parseInt(request.query.page) || 1;
+    const limit = parseInt(request.query.limit) || 3;
+    const offset = (page - 1) * limit
+
+    try {
+        const postagens = await Postagem.findAndCountAll({
+            limit,
+            offset
+        })
+        // console.log(page, limit, offset)
+        const totalPaginas = Math.ceil(postagens.count / limit)
+        response.status(200).json({
+            totalPostagens: postagens.count,
+            totalPaginas,
+            paginaAtual: page,
+            itemsPorPagina: limit,
+            proximaPagina: totalPaginas === 0 ? null: `http://localhost:3333/postagens?page=${page + 1}`,
+            tarefas: postagens.rows
+        })
+    } catch (error) {
+        response.status(500).json({ message: "Erro ao buscar as postagens" })
+    }
+}
+
--- backend/src/routes/postagemRoutes.js
+++ backend/src/routes/postagemRoutes.js
@@ -1,8 +1,9 @@
 import { Router } from "express"
-import { create } from "../controllers/postagemController.js"
+import { create, getAll } from "../controllers/postagemController.js"
 
 const router = Router()
 
 router.post("/", create)
+router.get("/", getAll)
 
 export default router
