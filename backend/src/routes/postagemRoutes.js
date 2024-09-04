e45a9f4def4d7d7fc718681cfa5cc92023283157
Ronaldo Duarte <ronaldoduarteoliveirarodrigues@gmail.com>
2024-09-04 10:10:00 -0300

    :sparkles: Feat: Rota da RF04 adicionada!

--- backend/src/routes/postagemRoutes.js
+++ backend/src/routes/postagemRoutes.js
@@ -1,10 +1,11 @@
 import { Router } from "express"
-import { create, getAll, getPostagemById } from "../controllers/postagemController.js"
+import { create, getAll, getPostagemById, updatePostagem } from "../controllers/postagemController.js"
 
 const router = Router()
 
 router.post("/", create)
 router.get("/", getAll)
 router.get("/:id", getPostagemById)
+router.put("/:id", updatePostagem)
 
 export default router
